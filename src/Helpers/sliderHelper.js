9bb16c623bbabd337e88a1e9fc9f2ebce0a856ff
jesus <jesusalvan2010@hotmail.com>
2020-08-09 16:10:37 -0500

    Bit Change

--- src/Helpers/sliderHelper.js
+++ src/Helpers/sliderHelper.js
@@ -7,7 +7,7 @@ export const separateSlider = subtopic => {
   let paragrahs = subtopic.split(".");
   if (paragrahs.length > TOTAL_PARAGRAPHS) {
     paragrahs.map((paragrah, index) => {
-      if (count == TOTAL_PARAGRAPHS || paragrahs.length == (index + 1)) {
+      if (count == TOTAL_PARAGRAPHS || paragrahs.length == index + 1) {
         tempArray.push(paragrahs.slice(index - count, index).join("."));
         count = 0;
       }
